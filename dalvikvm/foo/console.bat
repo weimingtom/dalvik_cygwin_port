089774bc3c239af5ce50056d29a102a5bf67228e
weimingtom <weimingtom@qq.com>
2017-10-13 14:28:30 +0800

    test failed

---
+++ dalvikvm/foo/console.bat
@@ -0,0 +1,4 @@
+@set path=%path%;D:\adt-bundle-windows-x86-20140624\sdk\platform-tools
+@set path=%path%;D:\adt-bundle-windows-x86-20140624\sdk\tools
+@set path=%path%;D:\adt-bundle-windows-x86-20140624\sdk\build-tools\23.0.2
+@cmd
